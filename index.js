bea550279f863e1e5eeb356362f19c151b59bb3e
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-26 08:27:11 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// code
+const record = [
+    {year: "2015", result: "W"},
+    {year: "2014", result: "N/A"},
+    {year: "2013", result: "L"},
+    //...
+]
+const sadly = 'undefined';
+
+
+function superbowlWin(record) {
+    for(const list in record) {
+    if (list.result === "W");
+    return list.year;
+    
+}
+    {record.find(superbowlWin);
+    }
+}
